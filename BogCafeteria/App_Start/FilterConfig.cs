458635cd8328d21cb84d2d8aad180ac36857e5cc
unknown <yurigurgenidze@yahoo.com>
2018-10-09 13:01:53 +0400

    please

---
+++ BogCafeteria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BogCafeteria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
